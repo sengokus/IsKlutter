d55c228ee67e4b49a95a67d830a739981e97ae95
HENERALPEPITO <95832346+HENERALPEPITO@users.noreply.github.com>
2023-12-30 04:24:02 +0800

    Add files via upload

---
+++ AllMessages.js
@@ -0,0 +1,263 @@
+import React, { useEffect, useState } from 'react';
+import {
+    VStack,
+    View,
+    HStack,
+    Text,
+    Heading,
+    Box,
+    Button,
+    ButtonText,
+    FormControl,
+    Input,
+    InputField,
+    ScrollView,
+} from '@gluestack-ui/themed';
+import { Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { TouchableOpacity } from 'react-native';
+import SearchHeaderBack from '../components/SearchHeaderBack.js';
+import ReceivedMessageBox from '../components/ReceivedMessageBox.js';
+
+import colors from '../config/colors.js';
+import { getFirestore, addDoc, collection, getDocs, query, where } from 'firebase/firestore';
+import Routes from '../components/constants/Routes.js';
+import { FIREBASE_APP } from '../../config/firebase';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+
+const db = getFirestore(FIREBASE_APP);
+const auth = getAuth();
+
+
+export default function AllMessagesPage( { user } ) {
+
+    const navigation = useNavigation();
+    const [usernames, setUsernames] = useState([]);
+    const [messages, setMessages] = useState([]);
+    const [sender, setSender] = useState([]);
+    
+
+    useEffect(() => {
+        const fetchAllUsernames = async () => {
+            try {   
+                const userCollection = collection(db, 'users');
+                const querySnapshot = await getDocs(userCollection);
+    
+                const allUsernames = [];
+                querySnapshot.forEach((doc) => {
+                    allUsernames.push(doc.data().username);
+                });
+    
+                setUsernames(allUsernames); // Set usernames to state
+            } catch (error) {
+                console.error('Error fetching user data:', error.message);
+            }
+        };
+    
+        fetchAllUsernames();
+    }, []);
+
+    useEffect(() => {
+        const fetchAllsender = async () => {
+            try {   
+                const userCollection = collection(db, 'users');
+                const querySnapshot = await getDocs(userCollection);
+    
+                const allSender = [];
+                querySnapshot.forEach((doc) => {
+                    allSender.push(doc.data().sender);
+                });
+    
+                setSender(allSender); // Set usernames to state
+            } catch (error) {
+                console.error('Error fetching user data:', error.message);
+            }
+        };
+    
+        fetchAllsender();
+    }, []);
+
+    useEffect(() => {
+        const fetchSenderandMessage = async () => {
+          try {
+            if (!auth || !auth.currentUser) {
+              setTimeout(fetchUserData, 1000);
+              return;
+            }
+      
+            const user = auth.currentUser;
+            const userCollection = collection(db, 'users');
+            const querySnapshot = await getDocs(query(userCollection, where('userID', '==', user.uid)));
+      
+            let loggedInUsername = null;
+      
+            querySnapshot.forEach((doc) => {
+              loggedInUsername = doc.data().username;
+            });
+      
+            const messagesCollection = collection(db, 'Messages');
+            const messagesSnapshot = await getDocs(messagesCollection);
+      
+            const senderMessagesMap = {}; // Object to store messages grouped by sender
+      
+            messagesSnapshot.forEach((doc) => {
+              const messageData = doc.data();
+              if (messageData.recipient === loggedInUsername) {
+                const sender = messageData.sender;
+                const message = messageData.message;
+      
+                if (!senderMessagesMap[sender]) {
+                  senderMessagesMap[sender] = {
+                    sender: sender,
+                    messages: [], // Initialize the array for the sender's messages
+                  };
+                }
+      
+                senderMessagesMap[sender].messages.push(message); // Push the message to the sender's array
+              }
+            });
+      
+            const filteredMessages = Object.values(senderMessagesMap); // Convert senderMessagesMap into an array
+      
+            setMessages(filteredMessages); // Set filtered messages to state
+          } catch (error) {
+            console.error('Error fetching data:', error.message);
+          }
+        };
+      
+        fetchSenderandMessage();
+      }, [auth]);
+      
+
+      
+    
+    
+    const renderUsernames = () => {
+        return usernames.map((username, index) => (
+          <TouchableOpacity key={index} onPress={() => handleUsernameClick(username)}>
+            <Text>{username}</Text>
+          </TouchableOpacity>
+        ));
+      };
+
+      const renderSender = () => {
+        return sender.map((senderName, index) => (
+          <TouchableOpacity key={index} onPress={() => handleUsernameClick(senderName)}>
+            <Text>{senderName}</Text>
+          </TouchableOpacity>
+        ));
+      };
+      
+      
+
+ 
+      
+      const renderSpecificMessage = () => {
+        return (
+          <View>
+            {messages.map((senderData, index) => (
+              <TouchableOpacity key={index} onPress={() => handleUsernameClick(senderData.sender)}>
+                <View>
+                  <Text style={{ fontSize: 18, fontWeight: 'bold', marginTop: 10 }}>
+                    Sender: {senderData.sender}
+                  </Text>
+                  <View>
+                    {senderData.messages.map((message, msgIndex) => (
+                      <View key={msgIndex} style={{ backgroundColor: 'white', padding: 10, marginVertical: 5 }}>
+                        <Text>Message: {message}</Text>
+                      </View>
+                    ))}
+                  </View>
+                </View>
+              </TouchableOpacity>
+            ))}
+          </View>
+        );
+      };
+      
+      
+      
+      
+      
+      
+    
+      const handleUsernameClick = (selectedUsername) => {
+        navigation.navigate(Routes.PRIVATEMESSAGE, { recipient: selectedUsername });
+      };
+      
+
+
+
+    const messagesData = [
+        { 
+            senderIcon: require("../../assets/img/sassa.jpg"),
+            senderName: "Sassa",
+            senderUsername: "@sassagurl",
+            message: "avail na po ang neon balls"
+        }, { 
+            senderIcon: require("../../assets/img/sassa.jpg"),
+            senderName: "Yasmin Marie Asistido",
+            senderUsername: "@kweenyasmin",
+            message: "hahhahaha"
+        }, { 
+            senderIcon: require("../../assets/img/sassa.jpg"),
+            senderName: "Juniper",
+            senderUsername: "@leng",
+            message: "mhine q"
+        },{ 
+            senderIcon: require("../../assets/img/sassa.jpg"),
+            senderName: "Ychan",
+            senderUsername: "@adele",
+            message: "ayaw q na"
+        },{ 
+            senderIcon: require("../../assets/img/sassa.jpg"),
+            senderName: "Ychan",
+            senderUsername: "@adele",
+            message: "hatdog"
+        }
+    ]
+
+    const renderAllMessages = () => {
+        return messagesData.map((message, index) =>
+            <ReceivedMessageBox 
+                key = {index}
+                senderIcon={message.senderIcon}
+                senderName={message.senderName}
+                senderUsername={message.senderUsername}
+                message={message.message}
+
+            />
+        );
+    }
+
+    return(
+        // Parent box
+        <Box w="100%" h="100%">
+        
+            {/*Search Bar*/}
+            <SearchHeaderBack posterUser="Sassa Girl" 
+            userIcon={require("../../assets/img/usericon.jpg")} />
+
+            {/* Messages Title */}
+            <VStack space="xs" alignItems="left" bgColor="$amber200" p="$3" width="100%">
+                <Heading lineHeight={40} fontSize="$3xl" fontWeight="$extrabold" 
+                    color={colors.secondary} m={2} >Messages</Heading>
+            </VStack>
+
+            {/* Container */}
+            <Box p="$0" maxWidth="100%" flex={0} bgColor="#cfcfcf">
+                <ScrollView>
+                    <HStack space={0} flexWrap="wrap">
+                                {renderUsernames()}
+                                {renderSpecificMessage()}
+                                {renderSender()}
+
+                    </HStack>
+                </ScrollView> 
+          </Box>
+        </Box>
+    )
+
+}
---
+++ PrivateMessage.js
@@ -0,0 +1,258 @@
+import React, { useEffect, useState } from 'react';
+import {
+    VStack,
+    HStack,
+    Text,
+    Heading,
+    Box,
+    Button,
+    ButtonText,
+    View,
+    FormControl,
+    Input,
+    InputField,
+    ScrollView,
+    Pressable
+} from '@gluestack-ui/themed';
+import { Alert } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { TouchableOpacity } from 'react-native';
+import SearchHeaderBack from '../components/SearchHeaderBack.js';
+import UserAvatar from '../components/Avatar.js';
+import SenderBox from '../components/SenderBox.js';
+import { serverTimestamp } from 'firebase/firestore';
+import colors from '../config/colors.js';
+import Routes from '../components/constants/Routes.js';
+import { getFirestore, addDoc, collection, getDocs, query, where } from 'firebase/firestore';
+import { FIREBASE_APP } from '../../config/firebase';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+const db = getFirestore(FIREBASE_APP);
+const auth = getAuth();
+
+
+export default function PrivateMessagePage() {
+    const route = useRoute();
+    const recipient = route.params?.recipient || '';
+    const [username, setUsername] = useState('');
+    const navigation = useNavigation();
+    const [messageContent, setMessageContent] = React.useState(''); // State to capture message content
+    const [messages, setMessages] = useState([]);
+
+
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+          try {
+            if (!auth || !auth.currentUser) {
+              setTimeout(fetchUserData, 1000);
+              return;
+            }
+    
+            const user = auth.currentUser;
+            const userCollection = collection(db, 'users');
+            const querySnapshot = await getDocs(query(userCollection, where('userID', '==', user.uid)));
+    
+            querySnapshot.forEach((doc) => {
+              setUsername(doc.data().username); 
+            });
+          } catch (error) {
+            console.error('Error fetching user data:', error.message);
+          }
+        };
+    
+        fetchUserData();
+      }, []);
+
+      
+
+  
+    const sendMessage = async () => {
+        try {
+          if (recipient && messageContent) {
+            const timestamp = serverTimestamp(); // Get the server timestamp
+          
+            const messageData = {
+              sender: username,
+              recipient: recipient,
+              message: messageContent,
+              timestamp: timestamp, // Include the timestamp in the message data
+            };
+    
+            // Add data to Firestore collection 'Messages' with the timestamp
+            const docRef = await addDoc(collection(db, 'Messages'), messageData);
+    
+            setMessageContent('');
+            alert('Message sent');
+            console.log('Document written with ID: ', docRef.id);
+          } else {
+            console.error('Recipient or message content missing');
+          }
+        } catch (error) {
+          console.error('Error adding document: ', error);
+        }
+      };
+
+      useEffect(() => {
+        const fetchSenderAndMessage = async () => {
+          try {
+            if (!auth || !auth.currentUser) {
+              setTimeout(fetchSenderAndMessage, 1000);
+              return;
+            }
+      
+            const user = auth.currentUser;
+            const userCollection = collection(db, 'users');
+            const querySnapshot = await getDocs(query(userCollection, where('userID', '==', user.uid)));
+      
+            let loggedInUsername = null;
+      
+            querySnapshot.forEach((doc) => {
+              loggedInUsername = doc.data().username;
+            });
+      
+            const messagesCollection = collection(db, 'Messages');
+            const messagesSnapshot = await getDocs(messagesCollection);
+      
+            const filteredMessages = [];
+      
+            messagesSnapshot.forEach((doc) => {
+              const messageData = doc.data();
+              const sender = messageData.sender;
+              const recipient = messageData.recipient;
+              const message = messageData.message;
+      
+              if (recipient === recipient && sender === loggedInUsername) {
+                filteredMessages.push({
+                  sender: sender,
+                  message: message,
+                });
+              }
+            });
+      
+            setMessages(filteredMessages);
+          } catch (error) {
+            console.error('Error fetching data:', error.message);
+          }
+        };
+      
+        fetchSenderAndMessage();
+      }, [auth, recipient]);
+      
+      
+
+    
+  
+
+  
+
+    const privMessagesData = [
+        { 
+            senderIcon: require("../../assets/img/sassa.jpg"),
+            senderName: "Sassa",
+            senderUsername: "@sassagurl",
+        }
+    ]
+    const renderMessages = () => {
+      if (!messages || messages.length === 0) {
+        return <Text>No messages available</Text>; // Handle the case where messages are empty
+      }
+    
+      return messages.map((message, index) => (
+        <SenderBox
+          key={index}
+          senderIcon={message.senderIcon}
+          senderName={message.senderName}
+          senderUsername={message.senderUsername}
+          message={message.message}
+        />
+      ));
+    };
+
+    
+    const renderSenderBox = () => {
+        return privMessagesData.map((privMessage, index) =>
+            <SenderBox
+                key = {index}
+                senderIcon={privMessage.senderIcon}
+                senderName={privMessage.senderName}
+                senderUsername={privMessage.senderUsername}
+                //message={message.message}
+
+            />
+        );
+    }
+
+    const renderSpecificMessage = () => {
+      if (!messages || messages.length === 0) {
+        return null; // Handle the case where messages are empty
+      }
+    
+      return (
+        <View>
+          {messages.map((senderData, index) => (
+            <View key={index}>
+              {senderData.message && (
+                  <View style={{ backgroundColor: 'white', padding: 10, marginVertical: 5 }}>
+                    <Text>{senderData.message}</Text>
+                  </View>
+                                )}
+            </View>
+          ))}
+        </View>
+      );
+    };
+    
+    
+
+    return(
+        // Parent box
+        <Box w="100%" h="100%">
+
+            {/*Search Bar*/}
+            <SearchHeaderBack userIcon={ require("../../assets/img/usericon.jpg") } back={navigation.goBack} />
+
+             {/* Messages Title */}
+            <VStack space="xs" alignItems="left" bgColor="$amber200" p="$3" width="100%">
+             <Heading lineHeight={40} fontSize="$3xl" fontWeight="$extrabold" color={colors.secondary} m={2}>
+                To: {recipient}
+            </Heading>
+            </VStack>
+
+            {/* Container */}
+            <ScrollView>
+            <HStack space={0} flexWrap="wrap">
+            {renderSpecificMessage()}
+
+                {/* Render message components or sender boxes */}
+            </HStack>
+
+            <Input bg={colors.white} borderColor={colors.secondary} h={75} w="72%" zIndex={0}>
+                    <InputField
+                        multiline={true}
+                        size="md"
+                        placeholder="Chat here..."
+                        value={messageContent}
+                        onChangeText={(text) => setMessageContent(text)} // Update message content state
+                            />
+                </Input>
+                        
+
+                <Button
+                    variant="solid"
+                    size="sm"
+                    bg={colors.secondary}
+                    borderRadius={8}
+                    onPress={sendMessage}
+                    ml={3}
+                >
+                    <Text color={colors.white} fontSize="$sm">Send</Text>
+                </Button>
+                        
+    </ScrollView>
+    
+        </Box>
+    )
+
+}   
